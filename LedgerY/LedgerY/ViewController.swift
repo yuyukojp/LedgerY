c8b78f5f8f876511dfa09de85e4630b44c87d1f4
金斗石 <jindoushi@fenrir-tec.com>
2021-11-08 17:12:49 +0800

    プロジェクト作成

---
+++ LedgerY/LedgerY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LedgerY
+//
+//  Created by 金斗石 on 2021/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
